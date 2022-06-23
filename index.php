9239df241a96e16b95607ec74b688159f0d6eaf4
Chanira Ranagna <81904593+chanira-rangana@users.noreply.github.com>
2022-06-24 03:36:05 +0530

    finish index.php

--- index.php
+++ index.php
@@ -1,4 +1,53 @@
 <?php require_once('inc/connection.php'); ?>
+<?php
+    // check for user form submission 
+    if(isset($_POST['submit'])){
+
+        $errors = array();
+
+        // check if the username password has been entered
+        if(!isset($_POST['email']) || strlen(trim($_POST['email'])) < 1 ) {
+            $errors[] = 'Username is Missing or Invalid';
+        }
+        if(!isset($_POST['password']) || strlen(trim($_POST['password'])) < 1 ) {
+            $errors[] = 'Password is Missing or Invalid';
+        }
+        // check if there any errors in the form
+        if(empty($errors)){
+            // save username and password into variables
+            $email = mysqli_real_escape_string($connection,$_POST['email']); 
+                //mysqli_real_escape_string is used for avoid sql query injection
+            $password = mysqli_real_escape_string($connection,$_POST['password']); 
+            $hashed_password = sha1($password);
+            
+            
+            // prepare database query
+            $query = "SELECT * FROM user 
+                        WHERE email='{$email}' 
+                        AND password='{$hashed_password}'
+                        LIMIT 1";
+
+            $result_set = mysqli_query($connection,$query);            
+            
+            // check if the user is valid  
+            if($result_set){
+                //query successfull
+
+                if(mysqli_num_rows($result_set) == 1){
+                    //valid user found
+                    // redirect to the user.php page
+                    header('Location: users.php');
+
+                }else {
+                    $errors[] = 'Invalid Username or Password';
+                }
+            }else {
+                $errors[] = 'Database query failed';
+            }
+        }
+        
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +62,12 @@
         <form action="index.php" method="POST">
             <fieldset>
                 <legend> <h1> Log In</h1></legend>
-                <!-- <p class="error">Invalid Username or Password</p> -->
+                
+                <?php
+                    if(isset($errors) && !empty($errors)){
+                        echo '<p class="error">Invalid Username or Password</p>';
+                    }
+                ?>
                 <p>
                     <label for="uname" >Username:</label>
                     <input type="email" name="email" id="uname" placeholder="Email address">

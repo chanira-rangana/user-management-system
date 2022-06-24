b5a2ae8d0c441c41a12632701fd3536bcbecb182
Chanira Ranagna <81904593+chanira-rangana@users.noreply.github.com>
2022-06-24 08:54:39 +0530

    create users table

--- users.php
+++ users.php
@@ -9,23 +9,66 @@ session_start();
         //checking didn't logged in
         header('Location: index.php');
     }
+
+    $user_list = '';
+
+    // getting the list of users
+    $query = 'SELECT * FROM user WHERE is_deleted=0 ORDER BY first_name';
+    $users = mysqli_query($connection,$query);
+    if($users){
+        while($user = mysqli_fetch_assoc($users)){
+            $user_list .= "
+                <tr>
+                    <td>{$user['first_name']}</td>    
+                    <td>{$user['last_name']}</td>    
+                    <td>{$user['last_login']}</td>    
+                    <td> <a href='modify-user.php?user_id={$user['id']}'>Edit</a> </td>    
+                    <td> <a href='delete-user.php?user_id={$user['id']}'>Delete</a> </td>    
+                </tr>
+            ";
+        }
+    }else {
+        echo "Database Query failed";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
     <title>User</title>
-</head>
-<body>
+  </head>
+  <body>
     <header>
-        <div class="appname">User Management System</div>
-        <div class="loggedin">Welcome <?php echo $_SESSION['first_name']; ?> ! <a href="logout.php">Log out</a> </div>
+      <div class="appname">User Management System</div>
+      <div class="loggedin">
+        Welcome
+        <?php echo $_SESSION['first_name']; ?>
+        ! <a href="logout.php">Log out</a>
+      </div>
     </header>
-    <h1>Users</h1>
-</body>
+
+    <main>
+      <h1>
+        Users <span><a href="add-user.php">+ Add New</a></span>
+      </h1>
+
+      <table class="userlist">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Last Login</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        <?php   
+            echo $user_list;
+        ?>
+      </table>
+    </main>
+  </body>
 </html>
 
 <?php mysqli_close($connection); ?>
